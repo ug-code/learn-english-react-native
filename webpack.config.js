bbc4ebc913c6fa8c0ead3dfebd5675b6b32162ca
ug <ugurgunes@windowslive.com>
2024-11-30 16:26:37 +0300

    Fixed for vercel

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
+
+module.exports = async function(env, argv) {
+    const config = await createExpoWebpackConfigAsync(env, argv);
+
+    // Customize Webpack config here if needed
+    return config;
+};
